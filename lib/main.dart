15cfaf9998f797d1401b60cb4dd33b58e7aea63e
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-05-23 11:27:25 -0300

    correcao

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_correcao/principal.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Principal(), //MUDAR A O NOME DA CLASSE DE "HOME" PARA "PRINCIPAL"
+    ));
+
